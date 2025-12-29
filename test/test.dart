cc7ea52b488573bff1152fc29c0876546a124e5f
Jimeet Shastri <jimeetshastri15@gmail.com>
2025-12-29 18:25:44 +0530

    tried integrating firebase but onHold for now

--- test/test.dart
+++ test/test.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:master_app/dashboard.dart';
 import 'package:master_app/login.dart';
 import 'package:master_app/main.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: LoginScreen(),   // <--- Directly open the page you want to test
+    home: DashBoardScreen(),   // <--- Directly open the page you want to test
   ));
 }
